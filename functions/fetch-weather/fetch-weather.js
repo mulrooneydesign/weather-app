3afd9cdaba5f9d94bebe93875ba989ab3b38ffab
Barry Mulrooney <barry.mulrooney@mulrooneydesign.ie>
2021-10-12 11:34:36 +0200

    reformat code style with new prettier rules

--- functions/fetch-weather/fetch-weather.js
+++ functions/fetch-weather/fetch-weather.js
@@ -1,24 +1,22 @@
 const axios = require('axios')
 
 const handler = async (event) => {
-  const { cityName, units} = event.queryStringParameters
+  const { cityName, units } = event.queryStringParameters
   const API_KEY = process.env.WEATHER_KEY
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=${units}`
 
   try {
-
     const { data } = await axios.get(url)
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     }
-
-  } catch  (error) {
-    const {status, statusText, headers, data } = error.response
-    return { 
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response
+    return {
       statusCode: status,
-      body: JSON.stringify({status, statusText, headers, data})
+      body: JSON.stringify({ status, statusText, headers, data }),
     }
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,17 @@
 import '../css/app.css'
 
 const stringToHTML = (string) => {
-	const parser = new DOMParser()
-	const doc = parser.parseFromString(string, 'text/html')
-	return doc.body.firstChild
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(string, 'text/html')
+  return doc.body.firstChild
 }
 
-const container = document.querySelector("#app");
+const container = document.querySelector('#app')
 
 const grid = stringToHTML(`<div class="grid"></div>`)
 const gridContainer = container.insertBefore(grid, null)
 
 const createInput = (container) => {
-
   const inputHtml = stringToHTML(`
   <div class="input-container">
     <label for="city">City</label>
@@ -27,31 +26,26 @@ const createInput = (container) => {
 
   button.addEventListener('click', (event) => {
     event.preventDefault()
-    getWeatherData(input.value);
+    getWeatherData(input.value)
   })
 }
 
 const createDataObject = (data) => {
+  let fetchedData = {
+    city: data.name,
+    longitude: data.coord.lon,
+    latitude: data.coord.lat,
+    description: data.weather[0].description,
+    main: data.weather[0].main,
+    temperature: data.main.temp,
+    temperature_max: data.main.temp_max,
+    temperature_min: data.main.temp_min,
+  }
 
-    let fetchedData = {
-      city: data.name,
-      longitude: data.coord.lon,
-      latitude: data.coord.lat,
-      description: data.weather[0].description,
-      main: data.weather[0].main,
-      temperature: data.main.temp,
-      temperature_max: data.main.temp_max,
-      temperature_min: data.main.temp_min,
-
-    }
-
-    return fetchedData
+  return fetchedData
 }
 
-
-
 const createApp = (data) => {
-
   const appHtml = stringToHTML(`
     <div class="city-container">
       <h1>${data.city}</h1>
@@ -64,27 +58,28 @@ const createApp = (data) => {
       <p><strong>Min Temperature:</strong> ${data.temperature_min}</p>
     </div>`)
 
-    gridContainer.insertBefore(appHtml, null)
+  gridContainer.insertBefore(appHtml, null)
 }
 
 const getWeatherData = (cityName) => {
-  const key = process.env.WEATHER_KEY;
 
   const units = 'metric'
 
-  fetch(`/.netlify/functions/fetch-weather/fetch-weather.js?cityName=${cityName}&units=${units}`)
+  fetch(
+    `/.netlify/functions/fetch-weather/fetch-weather.js?cityName=${cityName}&units=${units}`
+  )
     .then((response) => {
       return response.json()
     })
     .then((data) => {
-     createApp(createDataObject(data))
+      createApp(createDataObject(data))
     })
     .catch((error) => {
-     console.log(response.message, error)
-    });
-};
+      console.log(response.message, error)
+    })
+}
 
 window.onload = function () {
   getWeatherData('Barcelona')
   createInput(container)
-};
+}
