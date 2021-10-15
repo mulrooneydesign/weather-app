3bb3d3d719783e315d18cdc1ef59aa77e2834fe6
Barry Mulrooney <barry.mulrooney@mulrooneydesign.ie>
2021-10-15 21:41:35 +0200

    adjust ambient light

--- src/js/components/globe/components/light.js
+++ src/js/components/globe/components/light.js
@@ -2,7 +2,7 @@ import { AmbientLight } from "three";
 
 const createLight = () => {
 
-    const light = new AmbientLight(0xffffff, 0.8)
+    const light = new AmbientLight(0xffffff, 2.8)
     return light
 }
 
