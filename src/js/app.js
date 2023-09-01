a3475ab1b1324ebb4dd7b66a6d9a547d585d7bbd
Barry Mulrooney <barry.mulrooney@mulrooneydesign.ie>
2023-09-01 11:51:30 +0200

    Change notification

--- src/js/app.js
+++ src/js/app.js
@@ -1,42 +1,42 @@
-import '../css/app.css'
-import { stringToHTML } from './components/stringToHTML'
-import { createSwitch } from './components/createSwitch'
-import { createCity } from './components/createCity'
-import { createInput } from './components/createInput'
-import { createMessage } from './components/createMessage'
-import { Globe } from './components/globe/globe'
-import { createCanvasContainer } from './components/globe/components/canvas'
-import { myLoadingManager } from './components/globe/systems/myLoadingManager'
+import '../css/app.css';
+import { stringToHTML } from './components/stringToHTML';
+import { createSwitch } from './components/createSwitch';
+import { createCity } from './components/createCity';
+import { createInput } from './components/createInput';
+import { createMessage } from './components/createMessage';
+import { Globe } from './components/globe/globe';
+import { createCanvasContainer } from './components/globe/components/canvas';
+import { myLoadingManager } from './components/globe/systems/myLoadingManager';
 
 //Loads textures
-const loadedData = myLoadingManager()
+const loadedData = myLoadingManager();
 
 //App entry point in index.html
-const container = document.querySelector('#app')
+const container = document.querySelector('#app');
 
-const title = stringToHTML(`<h1 class="title">Weather App</h1>`)
-container.appendChild(title)
+const title = stringToHTML(`<h1 class="title">Weather App</h1>`);
+container.appendChild(title);
 
-const grid = stringToHTML(`<div class="grid"></div>`)
-container.appendChild(grid)
-const gridContainer =  document.querySelector('.grid')
+const grid = stringToHTML(`<div class="grid"></div>`);
+container.appendChild(grid);
+const gridContainer = document.querySelector('.grid');
 
 //Var to hold metric or imperial units
-let units = ''
+let units = '';
 
 //Creates an object to render the data for each fetched item
 const createDataObject = (data, units) => {
   if (stateProxy.isCelcius) {
-    units = 'metric'
+    units = 'metric';
   } else {
-    units = 'imperial'
+    units = 'imperial';
   }
 
-  let tempSymbol = ''
+  let tempSymbol = '';
   if (units === 'metric') {
-    tempSymbol = ' &#176C'
+    tempSymbol = ' &#176C';
   } else {
-    tempSymbol = ' &#176F'
+    tempSymbol = ' &#176F';
   }
 
   let fetchedData = {
@@ -48,86 +48,87 @@ const createDataObject = (data, units) => {
     temperature: data.main.temp + tempSymbol,
     temperature_max: data.main.temp_max + tempSymbol,
     temperature_min: data.main.temp_min + tempSymbol,
-  }
+  };
 
-  return fetchedData
-}
+  return fetchedData;
+};
 
 //Holds whether where in celcius or imperial
 const initialState = {
   isCelcius: true,
-}
+};
 
 //Watches for changes in temp units an calls createMessage when they are changed
 const stateHandler = {
   set: function (obj, prop, value) {
-    createMessage(container, null, 'Temperature units changed')
-    obj[prop] = value
-    return true
+    createMessage(
+      container,
+      null,
+      'Temperature units changed. Search again to see cities in the new units.'
+    );
+    obj[prop] = value;
+    return true;
   },
-}
+};
 
-const stateProxy = new Proxy(initialState, stateHandler)
+const stateProxy = new Proxy(initialState, stateHandler);
 
 //Var to hold cities
-const cities = []
-
+const cities = [];
 
 //Export switch for temp unit choice
 export const tempSwitcher = () => {
-  stateProxy.isCelcius = !stateProxy.isCelcius
-}
+  stateProxy.isCelcius = !stateProxy.isCelcius;
+};
 
 //Main Fetch get the data on each city from API and adds it to the cities object via createDataObject
 const getWeatherData = (cityName) => {
   if (stateProxy.isCelcius) {
-    units = 'metric'
+    units = 'metric';
   } else {
-    units = 'imperial'
+    units = 'imperial';
   }
 
   fetch(
     `/.netlify/functions/fetch-weather/fetch-weather.js?cityName=${cityName}&units=${units}`
   )
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((data) => {
-      cities.unshift(createDataObject(data))
-      createCity(cities, gridContainer)
-      globeInit(cities)
+      cities.unshift(createDataObject(data));
+      createCity(cities, gridContainer);
+      globeInit(cities);
     })
     .catch((error) => {
-      createMessage(container, null, error.error)
-    })
-}
-
+      createMessage(container, null, error.error);
+    });
+};
 
 //Globe variable
-let globe
+let globe;
 
 //Globe creator
 const globeInit = (cities) => {
-  
-  const globeContainer = createCanvasContainer()
+  const globeContainer = createCanvasContainer();
 
   if (globe) {
-    globe.update(cities)
+    globe.update(cities);
   } else {
-    globe = new Globe(globeContainer, cities, loadedData)
-    globe.render()
-    globe.start()
+    globe = new Globe(globeContainer, cities, loadedData);
+    globe.render();
+    globe.start();
   }
-}
+};
 
 //Start the app once everything is loaded
 window.onload = function () {
-  createSwitch(container, gridContainer)
-  getWeatherData('Barcelona')
-  createInput(container, gridContainer, getWeatherData)
+  createSwitch(container, gridContainer);
+  getWeatherData('Barcelona');
+  createInput(container, gridContainer, getWeatherData);
   createMessage(
     container,
     null,
     'Type the name of a city or town into the search field and click add.'
-  )
-}
+  );
+};
